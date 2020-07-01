c21dbae7058e021a4a75fb616d0a6e1af8def6c9
yeongminkim <oinochoe@naver.com>
2020-07-02 01:03:30 +0900

    alias 완료

---
+++ dist/alias.js
@@ -0,0 +1,19 @@
+function createUserAction2() {
+    return {
+        do() {},
+        name: '',
+    };
+}
+class UserImpl {
+    login() {
+        throw new Error('Method not implemented');
+    }
+}
+function checkUser(user) {
+    if (user.login()) {
+        return 'APPROVED';
+    } else {
+        return 'REJECTED';
+    }
+}
+//# sourceMappingURL=alias.js.map
