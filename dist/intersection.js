52d8505b1e42c8eab55b1c83fc29eb3051598207
yeongminkim <oinochoe@naver.com>
2020-07-02 00:19:19 +0900

    typescript guard assertion

--- dist/intersection.js
+++ dist/intersection.js
@@ -1,6 +1,28 @@
-function createUserAction(u, a) {
-    return Object.assign(Object.assign({}, u), a);
+function createUserAction(c, a) {
+    return Object.assign(Object.assign({}, c), a);
 }
 const u = createUserAction({ name: 'jay' }, { do() { } });
-console.log(u.do, u.name);
+function compare(x, y) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x === y ? 0 : x > y ? 1 : -1;
+    }
+    if (typeof x === 'string' && typeof y === 'string') {
+        return x.localeCompare(y);
+    }
+    throw Error('not supported type');
+}
+const v = compare(1, 1);
+console.log([3, 2, 1].sort(compare));
+console.log(['c', 'b', 'a'].sort(compare));
+function isAction(v) {
+    return v.do !== undefined;
+}
+function process(v) {
+    if (isAction(v)) {
+        v.do();
+    }
+    else {
+        console.log(v.name);
+    }
+}
 //# sourceMappingURL=intersection.js.map
--- src/intersection.ts
+++ src/intersection.ts
@@ -7,14 +7,16 @@ interface Action {
 }
 
 // & 로 표현하는 것을 인터섹션이라고 함
-function createUserAction(u: User, a: Action): User & Action {
+function createUserAction(c: User, a: Action): User & Action {
     // user와 action 타입을 합침..
-    return { ...u, ...a };
+    return { ...c, ...a };
 }
 
 const u = createUserAction({ name: 'jay' }, { do() {} });
-console.log(u.do, u.name);
 
+// 함수 오버로딩 사용으로 다음과같이 강타입 고정 가능
+function compare(x: string, y: string);
+function compare(x: number, y: number);
 // | 로 표현하는 것을 Union 타입이라고 함
 function compare(x: string | number, y: string | number) {
     if (typeof x === 'number' && typeof y === 'number') {
@@ -22,5 +24,22 @@ function compare(x: string | number, y: string | number) {
     }
     if (typeof x === 'string' && typeof y === 'string') {
         return x.localeCompare(y);
+    }
+    throw Error('not supported type');
+}
+
+const v = compare(1, 1);
+console.log([3, 2, 1].sort(compare));
+console.log(['c', 'b', 'a'].sort(compare));
+
+function isAction(v: User | Action): v is Action {
+    return (<Action>v).do !== undefined;
+}
+
+function process(v: User | Action) {
+    if (isAction(v)) {
+        v.do();
+    } else {
+        console.log(v.name);
     }
 }
